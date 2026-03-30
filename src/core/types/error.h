94ef87db921f061f234c852907cb7de8f3912f36
CelesteNostdlib <rnostdlib@gmail.com>
2026-03-30 14:03:18 -0700

    feat: extend ShellProcess error codes in documentation to include new error

--- src/core/types/error.h
+++ src/core/types/error.h
@@ -242,7 +242,7 @@ struct Error
 		Pty_NotSupported = 133,	  // PTY not supported on this platform
 
 		// -------------------------
-		// ShellProcess errors (135–136)
+		// ShellProcess errors (135–137)
 		// -------------------------
 		ShellProcess_CreateFailed = 135, // shell process creation failed
 		ShellProcess_NotSupported = 136, // shell not supported on this platform
